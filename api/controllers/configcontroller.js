6dbe2928ad6e2117156a4775e31451b736bc740b
Christian <chriamue@gmail.com>
2017-08-30 17:22:56 +0200

    updated controller

--- api/controllers/configcontroller.js
+++ api/controllers/configcontroller.js
@@ -4,5 +4,12 @@ var config = require('../../config.js');
 
 exports.createConfig = function (req, res) {
   //res.json({ text: "delivered" });
-  res.status(200).send('var config = ' + JSON.stringify(config));
+  var newconfig = {
+    account: process.env.ACCOUNT || config.account,
+    password: process.env.PASSWORD || config.password,
+    creditId: process.env.CREDITID || config.creditId,
+    ethhost: process.env.ETHHOST || config.ethhost,
+
+  }
+  res.status(200).send('var config = ' + JSON.stringify(newconfig));
 };
