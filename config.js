85059a5124a06a17f62dc70e199ed22b99e36ef9
Christian <chriamue@gmail.com>
2017-09-01 11:17:25 +0200

    changed contract id

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 module.exports = {
     account: '0x1b8aec0DBD737969E0319FdBA0Fb5221985aD637',
     password: 'smartcredit4',
-    creditId: '0x384f69faB58d90725c6D00a219FB9cd94f52f66F',
+    creditId: '0xD145aD212F2Ff61284b7e47877574d94ea03AC29',
     ethhost: 'http://localhost:8545',
     port: 41337,
     network_id: '*'
